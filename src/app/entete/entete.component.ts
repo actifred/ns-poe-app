5c3db548ff5fb3304ad6c83026730b942d8b84a2
Frederic Mandrea <fred@MacBook-Pro-de-Frederic.local>
2020-05-06 09:19:45 +0200

    Ajout entete

---
+++ src/app/entete/entete.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'nat-entete',
+    styleUrls: [ './entete.component.css' ],
+    templateUrl: './entete.component.html'
+})
+export class EnteteComponent {
+    public annee = 2020;
+}
