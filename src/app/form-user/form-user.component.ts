4682f48b7673e67a85ecd33abb02bd2f91e3787a
Frederic Mandrea <fred@MacBook-Pro-de-Frederic.local>
2020-05-07 10:54:45 +0200

    Gestion des erreurs de validateur

--- src/app/form-user/form-user.component.ts
+++ src/app/form-user/form-user.component.ts
@@ -15,8 +15,8 @@ export class FormUserComponent implements OnInit {
 
   ngOnInit(): void {
     this.formulaire = new FormGroup({
-      prenom: new FormControl('Paul'),
-      nom: new FormControl('', Validators.required),
+      prenom: new FormControl('Paul', this.champsPairEtPasTropLong),
+      nom: new FormControl('', Validators.compose([Validators.required, this.champsPairEtPasTropLong])),
       email: new FormControl(
         '',
         Validators.compose([
@@ -24,6 +24,18 @@ export class FormUserComponent implements OnInit {
           Validators.required
         ]))
     });
+  }
+
+  private champsPairEtPasTropLong(controle) {
+    let resultat:any = {};
+    if ((controle.value.trim().length % 2) !== 0) {
+      resultat.impair = true;
+    }
+    if (controle.value.trim().length > 8) {
+      resultat.troplong = true;
+    }
+    console.log(`Le resultat du test pour ${controle.value} est `, resultat);
+    return resultat;
   }
 
   public traiteFormulaire(valeur) {
